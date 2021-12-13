2b8dcad9cab70127c7053d6f992958a850f2386f
p.rosenthal <p.rosenthal@adito.de>
2021-12-13 11:51:01 +0100

    #2002619: Added a ToggleButton to all Ascii-Docs for changing the LineWrap option

---
+++ src/main/java/org/netbeans/asciidoc/actions/LineWrapAction.java
@@ -0,0 +1,45 @@
+package org.netbeans.asciidoc.actions;
+
+
+import org.jetbrains.annotations.NotNull;
+import org.netbeans.api.editor.settings.SimpleValueNames;
+import org.netbeans.modules.editor.indent.spi.CodeStylePreferences;
+import org.openide.text.DataEditorSupport;
+import org.openide.util.*;
+
+import javax.swing.*;
+import javax.swing.text.*;
+
+public class LineWrapAction extends JToggleButton
+{
+  private static final String _DISPLAY_NAME = NbBundle.getMessage(LineWrapAction.class, "LBL_LINE_WRAP_ACTION");
+  private static final String _LINE_WRAP_WORDS = "words";
+  private static final String _LINE_WRAP_NONE = "none";
+  private static final String _TEXT_LINE_WRAP = SimpleValueNames.TEXT_LINE_WRAP;
+  private final StyledDocument activeDocument;
+
+  public LineWrapAction(@NotNull Lookup pLookup)
+  {
+    super(_DISPLAY_NAME);
+    DataEditorSupport test = pLookup.lookup(DataEditorSupport.class);
+    activeDocument = test.getDocument();
+    this.setSelected(CodeStylePreferences.get(activeDocument).getPreferences().get(_TEXT_LINE_WRAP, "").equals("words")
+                         || CodeStylePreferences.get(activeDocument).getPreferences().get(_TEXT_LINE_WRAP, "").equals("chars"));
+
+    addActionListener(e -> {
+      AbstractButton button = (AbstractButton) e.getSource();
+      if (button.isSelected())
+      {
+        //both put functions are necessary. First one puts the key and second one refreshes the documents properties
+        CodeStylePreferences.get(activeDocument).getPreferences().put(_TEXT_LINE_WRAP, _LINE_WRAP_WORDS);
+        activeDocument.putProperty(_TEXT_LINE_WRAP, _LINE_WRAP_WORDS);
+      }
+      else
+      {
+        //both put functions are necessary. First one puts the key and second one refreshes the documents properties
+        CodeStylePreferences.get(activeDocument).getPreferences().put(_TEXT_LINE_WRAP, _LINE_WRAP_NONE);
+        activeDocument.putProperty(_TEXT_LINE_WRAP, _LINE_WRAP_NONE);
+      }
+    });
+  }
+}
